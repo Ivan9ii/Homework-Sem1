57c950f8e1420e727f551109bba44471e146464c
Ivan Ivanov <alecs.911@mail.ru>
2022-12-16 16:56:34 +0700

    Оно как то,но работает. Добавил задачу № 8 из ДЗ семинар 1

---
+++ Sem1_homework_task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число и узнайте какие числа в интервале от 1 до вашего числа являются четными");
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+for(int i = 1; i < N; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine($"Четное {i}");
+    }
+}
