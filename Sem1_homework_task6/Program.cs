b236035c1926f9a3e9b81f8e1bb7d7ddbe0f7239
Ivan Ivanov <alecs.911@mail.ru>
2022-12-16 14:00:22 +0700

    Добавил гитигнор(забыл до этого) и задачу №6 из дз семинар 1

---
+++ Sem1_homework_task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число и узнайте,являетя ли оно четным");
+int number;
+Console.WriteLine("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+
+if(number % 2 == 0)
+{
+    Console.WriteLine($"число {number} - четное");
+}
+else
+{
+    Console.WriteLine($"число {number} не является четным");
+}
